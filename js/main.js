a54b838a0c7b852cba458827039b101be6637dd7
MaiHuong64 <rain06042004@gmail.com>
2025-06-06 15:58:20 +0700

    feat: refactor location handling and update UI components for saved locations

--- js/main.js
+++ js/main.js
@@ -114,5 +114,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   checkLogin(uid, email, role);
 });
-
-
--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-import { TimKiem } from "./Search.js";
-// import { clear, displayWeather } from "./UI";
+import { TimKiem } from "./SearchService.js";
+import { SaveLocation, GetLoCations } from "./locationService.js"; 
+import { auth } from "./config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";    
+
+onAuthStateChanged(auth, async (user) => {
+
+    const list = document.getElementById("savedLocationsList");
+    const noLocation = document.getElementById("noLocations");
+
+    const locations = await GetLoCations(user);
+    console.log("Location: ", locations); 
+    
+    if(locations.length === 0){
+        noLocation.classList.remove("d-none");
+        list.classList.add("d-none");
+        list.innerHTML = ""; // Clear the list
+    }else{
+        noLocation.classList.add("d-none");
+        list.classList.remove("d-none");
+        list.innerHTML = ""; 
+        // Populate the list with saved locations
+
+         locations.forEach((location) => {
+          const li = document.createElement("li");
+          li.className = "list-group-item";
+          li.textContent = location;
+          list.appendChild(li);
+
+        });
+    }
+});
+
 
 document.getElementById("btn-Search").addEventListener("click", TimKiem);
+document.getElementById('btn-SaveLocation').addEventListener("click", SaveLocation);
+
