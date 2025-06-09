37777fd793fb558d7c3d5db67ba3712f365a848b
MaiHuong64 <rain06042004@gmail.com>
2025-06-09 17:01:26 +0700

    feat: enhance user profile management and image upload functionality; refactor account handling and update user data in Firestore

--- js/admin.js
+++ js/admin.js
@@ -2,8 +2,7 @@ import { getUsers } from "./users.js";
 import { getLogs } from "./logs.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
-    getUsers();
-});
-window.addEventListener("DOMContentLoaded", async () => {
+    // getUsers();
     getLogs();
+    await getUsers();
 });
